f1a1e9712ba0a2bd4673601038406e7a3e08899a
Christian Miranda <cmiranda@caltech.edu>
2025-04-23 22:43:49 -0700

    Added mau.vhd

--- sh2_dmau.vhd
+++ sh2_dmau.vhd
@@ -5,8 +5,10 @@
 --
 --  This is an implementation of the SH-2's DMAU using Glen A. George's 
 --  generic memory access unit. The SH-2 is a Princeton architecture CPU
---  where program memory can only be read. The following 11 addressing modes
---  are supported by the SH-2 instruction set.
+--  with only a shared memory and data bus. This DMAU entity calculates
+--  memory addresses based on the input control signals, and contains
+--  the GBR (General Base Register). The SH-2 has the following addressing 
+--  modes:
 --
 --    1.  Direct Register Addressing
 --    2.  Indirect Register Addressing
@@ -27,13 +29,17 @@
 -- locations. As such, the DMAU will not do direct register addressing, since
 -- that is done by the register array, nor will it do immediate addressing, 
 -- which is done by the control unit during instruction decoding. Finally,
--- there are not safeguards agaisnt addressing modes which do not actually
+-- there are not safeguards against addressing modes which do not actually
 -- exist in the SH-2.
 --
 --  Revision History:
 --		16 April 25		Chris M. Initial reivision.
 --    23 April 25   Chris M. Add seperate calculated offsets to AddrOff matrix
 --                           instead of muxing between a single offset.
+--
+--    23 April 25   Chris M. Removed 12-bit offset input and OffExtendSel 
+--                           because 12-bit offsets and sign-extension is not
+--                           used for memory accesses, only for relative jumps.
 --
 -- [TODO]:
 --    - Don't allow inputs that don't correspond to addressing modes.
@@ -61,16 +67,10 @@ package SH2DmauConstants is
   constant BaseSel_PC  : std_logic_vector(1 downto 0)  := "10"; -- 2
 
   -- IndexSel constants.
-  constant IndexSel_NONE   : std_logic_vector(2 downto 0) := "000";   -- 0
-  constant IndexSel_OFF4   : std_logic_vector(2 downto 0) := "001";   -- 1
-  constant IndexSel_OFF8   : std_logic_vector(2 downto 0) := "010";   -- 2
-  constant IndexSel_OFF12  : std_logic_vector(2 downto 0) := "011";   -- 3
-  constant IndexSel_R0     : std_logic_vector(2 downto 0) := "100";   -- 4
-  constant IndexSel_REG    : std_logic_vector(2 downto 0) := "101";   -- 5
-
-  -- OffExtendSel constants. Zero-extend or sign-extend the offset into a 32-bit word.
-  constant OffExtendSel_ZERO : std_logic := '0';
-  constant OffExtendSel_SIGN : std_logic := '1';
+  constant IndexSel_NONE   : std_logic_vector(1 downto 0) := "00";   -- 0
+  constant IndexSel_OFF4   : std_logic_vector(1 downto 0) := "01";   -- 1
+  constant IndexSel_OFF8   : std_logic_vector(1 downto 0) := "10";   -- 2
+  constant IndexSel_R0     : std_logic_vector(1 downto 0) := "11";   -- 3
 
   -- OffsetScalarSel constant. What to scale the offset (or increment value) by.
   --
@@ -98,7 +98,7 @@ use work.all;
 use work.MemUnitConstants.all; -- memory access unit constants for pre/post inc/dec.
 use work.SH2Constants.all;     -- global SH-2 constants.
 use work.array_type_pkg.all;   -- 2D Array of std_logic (VHDL-2008 only).
-use work.SH2DmauConstants.all;
+use work.SH2DmauConstants.all;  
 
 
 -- SH2Dmau
@@ -136,10 +136,6 @@ use work.SH2DmauConstants.all;
 -- | 8.  PC relative addressing with displacement         | Addr = @(PC + zero_extend(Off8) * 2)                |
 -- | -                                                    | Addr = @((PC & 0xFFFFFFFC) + zero_extend(Off8) * 4) |
 -- +------------------------------------------------------+-----------------------------------------------------+
--- | 9. PC relative addressing                            | Addr = @(PC + sign_extend(Off8) * 2)                |
--- | -                                                    | Addr = @(PC + sign_extend(Off12) * 2)               |
--- | -                                                    | Addr = @(PC + Rn)                                   |
--- +------------------------------------------------------+-----------------------------------------------------+
 --
 -- "Base" sources are the sources to the left hand side of an address 
 -- calculation, and consist of Rn (a register), GBR (Global Base Register), or 
@@ -153,22 +149,21 @@ use work.SH2DmauConstants.all;
 --  RegSrc       -   Input register. One of three possible base sources.
 --  R0Src        -   R0 Register input.
 --  PCSrc        -   Program Counter Source.
+--  GBRIn        -   GBR input.
+--  GBRWriteEn   -   GBR write enable. Active high.
 --  Off4         -   4-Bit Offset.
 --  Off8         -   8-Bit Offset.
---  Off12        -   12-Bit Offset.
---  BaseSel      -   Which base register source to select (Reg, GBR, or PC).
---  IndexSel     -   Which index source to select (None, Off4, Off8, Off12, Rn,
---                   or R0). Note that Off4, Off8, Off12 are zero or sign 
---                   extended before being added to the base.
+--  BaseSel      -   Which base register source to select.
+--  IndexSel     -   Which index source to select (None, Off4, Off8, Rn,
+--                   or R0). Note that Off4, Off8 are zero extended before 
+--                   being added to the base.
 --
---  OffExtendSel -   Zero or sign extend the offset into a 32-bit word.
 --  OffScalar    -   What to scale the offset by (1, 2, 4)
 --  IncDecSel    -   Post Increment or PreDecrement the base. Note that the
 --                   amount added or subtracted is scaled ny OffScalar. Note 
 --                   that even when this is set to NONE, an 
 --                   incremented/decremented value is output to AddrSrcOut as a
 --                   result of the generic memory access unit design.
---  GBRIn        -   GBR input.
 --  Clk          -   Clk input.
 --  
 -- Outputs:
@@ -182,12 +177,12 @@ entity SH2Dmau is
     RegSrc       : in     std_logic_vector(SH2_WORDSIZE - 1 downto 0);
     R0Src        : in     std_logic_vector(SH2_WORDSIZE - 1 downto 0);
     PCSrc        : in     std_logic_vector(SH2_WORDSIZE - 1 downto 0);
+    GBRIn        : in     std_logic_vector(SH2_WORDSIZE - 1 downto 0);
+    GBRWriteEn   : in     std_logic;
     Off4         : in     std_logic_vector(3 downto 0);
     Off8         : in     std_logic_vector(7 downto 0);
-    Off12        : in     std_logic_vector(11 downto 0);
     BaseSel      : in     std_logic_vector(1 downto 0);
     IndexSel     : in     std_logic_vector(2 downto 0);
-    OffExtendSel : in     std_logic;
     OffScalarSel : in     std_logic_vector(1 downto 0);
     IncDecSel    : in     std_logic_vector(1 downto 0);
     Clk          : in     std_logic;
@@ -208,14 +203,6 @@ architecture structural of SH2Dmau is
       result(slv'range) := slv;
     end function;
 
-    -- SignExtend a std_logic_vector into an SH2_WORDSIZE std_logic_vector.
-    --
-    pure function SignExtend(slv : std_logic_vector) return std_logic_vector is
-      variable result : std_logic_vector(SH2_WORDSIZE - 1 downto 0);
-    begin
-      result := (others => slv(slv'left));
-      result(slv'range)  := slv;
-    end function;
 
     -- shift_left is defined for unsigned/signed types only; wrap for slv.
     --
@@ -232,13 +219,13 @@ architecture structural of SH2Dmau is
     -- MemUnit generic constants.
     --
     constant SRCCNT       : integer := 3;  -- Number of vectors in AddrSrc matrix. 
-    constant OFFSETCNT    : integer := 9;  -- Number of vectors in AddrOff matrix.
+    constant OFFSETCNT    : integer := 4;  -- Number of vectors in AddrOff matrix.
     constant MAXINCDECBIT : integer := 2;  -- The maximum index of the bit of 
                                            -- the calculted address that we want 
                                            -- to increment or decrement.  
 
     -- DMAUAddrSrc.
-    -- Consists of RegSrc, GBR, and PC. Note that this is a one-to-one mapping
+    -- Consists of RegSrc, GBR, and PC. Note that there is a one-to-one mapping
     -- between the BaseSel constants.
     --
     signal DMAUAddrSrc : std_logic_array(SRCCNT - 1 downto 0)(SH2_WORDSIZE - 1 downto 0);
@@ -252,15 +239,10 @@ architecture structural of SH2Dmau is
     
 
     -- DMAUOffsetSel constants.
-    constant ZERO                 : integer := 0;
-    constant OFF4_ZERO_EXTENDED   : integer := 1;
-    constant OFF8_ZERO_EXTENDED   : integer := 2;
-    constant OFF12_ZERO_EXTENDED  : integer := 3;
-    constant R0                   : integer := 4;
-    constant OFF4_SIGN_EXTENDED   : integer := 5;
-    constant OFF8_SIGN_EXTENDED   : integer := 6;
-    constant OFF12_SIGN_EXTENDED  : integer := 7;
-    constant REG                  : integer := 8;
+    constant ZERO   : integer := 0;
+    constant OFF4   : integer := 1;
+    constant OFF8   : integer := 2;
+    constant R0     : integer := 3;
     signal 	 DMAUOffsetSel        : integer range OFFSETCNT - 1 downto 0;
 
     signal DMAUAddrOff : std_logic_array(OFFSETCNT - 1 downto 0)(SH2_WORDSIZE - 1 downto 0);
@@ -289,22 +271,20 @@ architecture structural of SH2Dmau is
     -- Zero/sign extedned offsets.
     signal Off4ZeroExtended   : std_logic_vector(SH2_WORDSIZE - 1 downto 0);
     signal Off8ZeroExtended   : std_logic_vector(SH2_WORDSIZE - 1 downto 0);
-    signal Off12ZeroExtended  : std_logic_vector(SH2_WORDSIZE - 1 downto 0);
-    signal Off4SignExtended   : std_logic_vector(SH2_WORDSIZE - 1 downto 0);
-    signal Off8SignExtended   : std_logic_vector(SH2_WORDSIZE - 1 downto 0);
-    signal Off12SignExtended  : std_logic_vector(SH2_WORDSIZE - 1 downto 0);
 
 begin
 
   WriteGBR : process(Clk)
   begin
     if rising_edge(Clk) then
-      GBR <= GBRIn;
+      if (GBRWriteEn = '1') then
+        GBR <= GBRIn;
+      end if;
     end if;
   end process WriteGBR;
 
-
   GBROut <= GBR;
+
   -- DMAUAddrSrc  ------------------------------------------------------------
 
   -- The low two bits of the PC are masked if PC is selected as the base address, 
@@ -323,65 +303,31 @@ begin
   DMAUSrcSel <= to_integer(unsigned(BaseSel));
 
   -- DMAUAddrOff  ------------------------------------------------------------
-
   Off4ZeroExtended  <= ZeroExtend(Off4);
   Off8ZeroExtended  <= ZeroExtend(Off8);
-  Off12ZeroExtended <= ZeroExtend(Off12);
 
-  Off4SignExtended  <= SignExtend(Off4);
-  Off8SignExtended  <= SignExtend(Off8);
-  Off12SignExtended <= SignExtend(Off12);
   
   -- Populate the DMAUAddrOff matrix. 
   DMAUAddrOff(ZERO) <= (others => '0');
 
-  DMAUAddrOff(OFF4_ZERO_EXTENDED)   <= 
-      shift_left_slv(Off4ZeroExtended, to_integer(unsigned(OffScalarSel)));
+  DMAUAddrOff(OFF4) <= 
+    shift_left_slv(Off4ZeroExtended, to_integer(unsigned(OffScalarSel)));
 
-  DMAUAddrOff(OFF8_ZERO_EXTENDED)   <= 
+  DMAUAddrOff(OFF8)   <= 
       shift_left_slv(Off8ZeroExtended, to_integer(unsigned(OffScalarSel)));
-
-  DMAUAddrOff(OFF12_ZERO_EXTENDED)  <= 
-      shift_left_slv(Off12ZeroExtended, to_integer(unsigned(OffScalarSel)));
       
   DMAUAddrOff(R0) <= R0Src;
 
-  DMAUAddrOff(OFF4_SIGN_EXTENDED)   <= 
-      shift_left_slv(Off4ZeroExtended, to_integer(unsigned(OffScalarSel)));
-
-  DMAUAddrOff(OFF8_SIGN_EXTENDED)   <= 
-      shift_left_slv(Off8ZeroExtended, to_integer(unsigned(OffScalarSel)));
-
-  DMAUAddrOff(OFF12_SIGN_EXTENDED)  <= 
-      shift_left_slv(Off12ZeroExtended, to_integer(unsigned(OffScalarSel)));
-
-  DMAUAddrOff(REG) <= RegSrc;
 
   -- DMAUOffsetSel  ----------------------------------------------------------
   DMAUOffsetSel <= 
       ZERO when (IndexSel = IndexSel_NONE) else
 
-      OFF4_ZERO_EXTENDED when (IndexSel = IndexSel_OFF4) and 
-                              (OffExtendSel = OffExtendSel_ZERO) else
+      OFF4 when (IndexSel = IndexSel_OFF4) and 
 
-      OFF8_ZERO_EXTENDED when (IndexSel = IndexSel_OFF8) and
-                              (OffExtendSel = OffExtendSel_ZERO) else
+      OFF8 when (IndexSel = IndexSel_OFF8) else
 
-      OFF12_ZERO_EXTENDED when (IndexSel = IndexSel_OFF12) and
-                               (OffExtendSel = OffExtendSel_ZERO) else    
-  
       R0 when (IndexSel = IndexSel_R0) else
-
-      OFF4_SIGN_EXTENDED when (IndexSel = IndexSel_OFF4) and
-                              (OffExtendSel = OffExtendSel_SIGN) else
-
-      OFF8_SIGN_EXTENDED when (IndexSel = IndexSel_OFF8) and
-                              (OffExtendSel = OffExtendSel_SIGN) else
-
-      OFF12_SIGN_EXTENDED when (IndexSel = IndexSel_OFF12) and
-                               (OffExtendSel = OffExtendSel_SIGN) else
-
-      REG                 when (IndexSel = IndexSel_REG) else
   
       DMAUOffsetSel;
       
