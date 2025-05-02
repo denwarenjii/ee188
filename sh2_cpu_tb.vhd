aa60a8d3082c1919bff46f2df6aa53055f747eb9
Zachary Huang <55601738+zack466@users.noreply.github.com>
2025-05-02 13:38:34 -0700

    read in binary files into cpu memory

--- sh2_cpu_tb.vhd
+++ sh2_cpu_tb.vhd
@@ -63,12 +63,12 @@ begin
 
     mem : entity work.MEMORY32x32
     generic map (
-        MEMSIZE => 256,
-        -- four contiguous blocks of memory (256 bytes each)
-        START_ADDR0 => 16#000#,
-        START_ADDR1 => 16#100#,
-        START_ADDR2 => 16#200#,
-        START_ADDR3 => 16#300#
+        MEMSIZE => 1024,
+        -- four contiguous blocks of memory (1024 bytes each)
+        START_ADDR0 => 16#0000#,
+        START_ADDR1 => 16#1000#,
+        START_ADDR2 => 16#2000#,
+        START_ADDR3 => 16#3000#
     )
     port map (
         RE0 => RE0,
@@ -85,6 +85,67 @@ begin
 
     process
 
+        -- Assumes that address is word-aligned
+        procedure WriteWord(address : unsigned; data : std_logic_vector) is
+        begin
+            AB <= std_logic_vector(address);
+
+            -- Shift word of data over to correct location
+            DB(15 downto 0)  <= data when address mod 4 = 0 else (others => 'X');
+            DB(31 downto 16) <= data when address mod 4 = 2 else (others => 'X');
+
+            -- Write only the word being addressed
+            WE0 <= '0' when address mod 4 = 0 else '1';
+            WE1 <= '0' when address mod 4 = 0 else '1';
+            WE2 <= '0' when address mod 4 = 2 else '1';
+            WE3 <= '0' when address mod 4 = 2 else '1';
+
+            wait for 5 ns;  -- wait for signal to propagate
+
+            -- Disable writing
+            WE0 <= '1';
+            WE1 <= '1';
+            WE2 <= '1';
+            WE3 <= '1';
+
+            wait for 5 ns;  -- wait for signal to propagate
+        end procedure;
+
+        -- Reading in a binary file byte-by-byte
+        -- Reference: https://stackoverflow.com/a/42581872
+        procedure LoadProgram(path : string) is
+            type char_file_t is file of character;
+            file char_file : char_file_t;
+            variable char_v : character;
+            subtype byte_t is natural range 0 to 255;
+            variable byte_v : byte_t;
+
+            variable curr_opcode : std_logic_vector(15 downto 0);
+            variable curr_pc     : unsigned(31 downto 0);
+        begin
+            curr_pc := to_unsigned(0, 32);
+
+            -- read file as "characters" to get individual bytes
+            file_open(char_file, path);
+            while not endfile(char_file) loop
+                -- read low byte of instruction
+                read(char_file, char_v);
+                byte_v := character'pos(char_v);
+                curr_opcode(7 downto 0) := std_logic_vector(to_unsigned(byte_v, 8));
+
+                -- read high byte of instruction
+                read(char_file, char_v);
+                byte_v := character'pos(char_v);
+                curr_opcode(15 downto 8) := std_logic_vector(to_unsigned(byte_v, 8));
+
+                -- Write instruction word into memory
+                WriteWord(curr_pc, curr_opcode);
+
+                curr_pc := curr_pc + 2;
+            end loop;
+            file_close(char_file);
+        end procedure;
+
         procedure Tick is
         begin
             clock <= '0';
@@ -95,6 +156,8 @@ begin
 
     begin
         report "Hello, world!";
+
+        LoadProgram("asm/hello.bin");
 
         END_SIM <= TRUE;
         wait;
