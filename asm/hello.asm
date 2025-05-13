c7df1078c32be26d1df2502574245b4470fc73f9
Christian Miranda <cmiranda@caltech.edu>
2025-05-12 18:45:15 -0700

    WIP

--- asm/hello.asm
+++ asm/hello.asm
@@ -1,7 +1,7 @@
 ProgramStart:
     ; Write 0000000F to address 00000000
     MOV #15, R0;
-    MOV #0, R1;
+    MOV #$00, R1;
     MOV R0, @R1;
     ; Quit test program
     MOV #-4, R0;
--- asm/mov_wl_at_disp_pc_rn.asm
+++ asm/mov_wl_at_disp_pc_rn.asm
@@ -14,38 +14,51 @@
 ;
 
 ProgramStart:
-  ;MOV #imm, Rn is limited from $00 to $FF
+  ;MOV #imm, Rn is limited from $00 to $FF. immediates are sign-extended.
 
   ;We will move 
-  ;   0xAA at address 0x7A, 
-  ;   0xBB at address 0x7B, 
-  ;   0xCC at address 0x7C,
-  ;   0xDD at address 0x7D
+  ;   0xAA at address 0x007A, 
+  ;   0xBB at address 0x007B, 
+  ;   0xCC at address 0x007C,
+  ;   0xDD at address 0x007D
 
-  MOV   #$80, R1  ; PC is 0x02
+  MOV   #$AA, R1   ; PC is 0x00
+  MOV   #$7A, R0   ; PC is 0x02
+  MOV.B R1, @R0    ; PC is 0x04
 
-  MOV   #$7A, R0  ; PC is 0x04
-  MOV.B R1, @R0   ; PC is 0x06
+  MOV   #$BB, R1   ; PC is 0x06
+  MOV   #$7B, R0   ; PC is 0x08
+  MOV.B R1, @R0    ; PC is 0x0A
 
-  MOV   #$7B, R0  ; PC is 0x08
-  MOV.B R1, @R0   ; PC is 0x0A
+  MOV   #$CC, R1   ; PC is 0x0C
+  MOV   #$7C, R0   ; PC is 0x0E
+  MOV.B R1, @R0    ; PC is 0x10
 
-  MOV   #$7C, R0  ; PC is 0x0C
-  MOV.B R1, @R0   ; PC is 0x0E
-
-  MOV   #$7D, R0  ; PC is 0x10
-  MOV.B R1, @R0   ; PC is 0x18
+  MOV   #$DD, R1   ; PC is 0x12
+  MOV   #$7D, R0   ; PC is 0x14
+  MOV.B R1, @R0    ; PC is 0x16
   
+  ; NOP              ; PC is 0x18. Needed for alignment.
 
-  ; disp is zero extended and doubled for MOV.W
-  ; 0x20 + d*2 = 0x7A -> d = 0x2D
-  MOV.W ($2D, PC), R2 ; PC is 0x20
+  ; ; disp is zero extended and doubled for MOV.W
+  ; ; 0x1A + d*2 = 0x7A -> d = 0x30
+  ; MOV.W @($30, PC), R2 ; PC is 0x1A
 
-  ; disp is zero extended and quadrupled for MOV.L
-  ; 0x22 + d*4 = 0x7A -> d = 0x16
-  MOV.L ($16, PC), R3 ; PC is 0x22
+  ; NOP              ; PC is 0x1C
 
-Done:
-  ; The test bench interprets a read of 0xFFFFFFFC (-4) as system exit.
-  MOV #-4, R0;
-  MOV.B R0, @R0
+  ; ; disp is zero extended and quadrupled for MOV.L
+  ; ; 0x1E + d*4 = 0x7A -> d = 0x17
+  ; MOV.L @($17, PC), R3 ; PC is 0x1E
+
+  ; ; Move R2 and R3 into memory to check for correctness.
+  ; MOV #$00, R0   ; 0xFFFFAABB expected at 0x00000000
+  ; MOV.W R2, @R0
+
+  ; MOV #$04, R0   ; 0xAABBCCDD expected at 0x00000004
+  ; MOV.W R3, @R0
+
+  
+; Done:
+;   ; The test bench interprets a read of 0xFFFFFFFC (-4) as system exit.
+;   MOV #-4, R0;
+;   MOV.B R0, @R0
